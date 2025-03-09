f1822c9c7cf80546eb73794f48f173f353bada17
sung2708 <aki18082002@gmail.com>
2025-03-09 22:47:36 +0700

    add 2 problem in Mathematics

--- Mathematics/findPoint.cpp
+++ Mathematics/findPoint.cpp
@@ -17,88 +17,97 @@ vector<string> split(const string &);
  *  4. INTEGER qy
  */
 
-vector<int> findPoint(int px, int py, int qx, int qy) {
+vector<int> findPoint(int px, int py, int qx, int qy)
+{
 
-  vector<int> sol;
-  sol.push_back(2 * qx - px);
-  sol.push_back(2 * qy - py);
+    vector<int> sol;
+    sol.push_back(2 * qx - px);
+    sol.push_back(2 * qy - py);
 
-  return sol;
+    return sol;
 }
 
-int main() {
-  ofstream fout(getenv("OUTPUT_PATH"));
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-  string n_temp;
-  getline(cin, n_temp);
+    string n_temp;
+    getline(cin, n_temp);
 
-  int n = stoi(ltrim(rtrim(n_temp)));
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-  for (int n_itr = 0; n_itr < n; n_itr++) {
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
+    for (int n_itr = 0; n_itr < n; n_itr++)
+    {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
 
-    vector<string> first_multiple_input =
-        split(rtrim(first_multiple_input_temp));
+        vector<string> first_multiple_input =
+            split(rtrim(first_multiple_input_temp));
 
-    int px = stoi(first_multiple_input[0]);
+        int px = stoi(first_multiple_input[0]);
 
-    int py = stoi(first_multiple_input[1]);
+        int py = stoi(first_multiple_input[1]);
 
-    int qx = stoi(first_multiple_input[2]);
+        int qx = stoi(first_multiple_input[2]);
 
-    int qy = stoi(first_multiple_input[3]);
+        int qy = stoi(first_multiple_input[3]);
 
-    vector<int> result = findPoint(px, py, qx, qy);
+        vector<int> result = findPoint(px, py, qx, qy);
 
-    for (size_t i = 0; i < result.size(); i++) {
-      fout << result[i];
+        for (size_t i = 0; i < result.size(); i++)
+        {
+            fout << result[i];
 
-      if (i != result.size() - 1) {
-        fout << " ";
-      }
+            if (i != result.size() - 1)
+            {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
     }
 
-    fout << "\n";
-  }
+    fout.close();
 
-  fout.close();
-
-  return 0;
+    return 0;
 }
 
-string ltrim(const string &str) {
-  string s(str);
+string ltrim(const string &str)
+{
+    string s(str);
 
-  s.erase(s.begin(),
-          find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+    s.erase(s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
 
-  return s;
+    return s;
 }
 
-string rtrim(const string &str) {
-  string s(str);
+string rtrim(const string &str)
+{
+    string s(str);
 
-  s.erase(
-      find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-      s.end());
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
 
-  return s;
+    return s;
 }
 
-vector<string> split(const string &str) {
-  vector<string> tokens;
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
 
-  string::size_type start = 0;
-  string::size_type end = 0;
+    string::size_type start = 0;
+    string::size_type end = 0;
 
-  while ((end = str.find(" ", start)) != string::npos) {
-    tokens.push_back(str.substr(start, end - start));
+    while ((end = str.find(" ", start)) != string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
 
-    start = end + 1;
-  }
+        start = end + 1;
+    }
 
-  tokens.push_back(str.substr(start));
+    tokens.push_back(str.substr(start));
 
-  return tokens;
+    return tokens;
 }
